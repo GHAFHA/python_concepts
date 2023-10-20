2a99454d0b4da60db0a9834a7f2a40b1990806c0
Skribb11es <william.davis@aa.com>
2023-10-19 22:42:29 -0500

    Modularity or smth idk

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 plotFunc = plot_functions(file_path1="data/aerodata.csv",file_path2="data/aerodata2.csv")
 
-print(plotFunc.compare_min_max_mean())
+plotFunc.display_plot("data/aerodata.csv", [["Rear Rideheight", "ClA"], ["Front Rideheight", "ClA"]])
